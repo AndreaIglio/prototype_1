b0721bb288bc526f7f79a3bccd06ea794a89d506
Andrea Iglio <andrea.iglio@gmail.com>
2021-01-04 18:00:35 +0100

    Refactored "#my_resume" code using VueJs

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,27 @@ var app = new Vue({
       "assets/img/skills/vue-9.svg",
       "assets/img/skills/bootstrap-4.svg",
     ],
+    resume_card: "resume_card",
+    formation: [
+      {
+        subject: "Web Developer Bootcamp",
+        year: "2020 - 2021",
+        description:
+          "Full Stack Web Developer bootcamp of six months in which i studied different programming languages and frameworks such as the ones i call my skills",
+      },
+      {
+        subject: "High School Degree in Turin, Italy",
+        year: "2013",
+        description: "Scientific High School Degree",
+      },
+    ],
+    workexperience: [
+      {
+        position: "Swag",
+        year: "2013/2015",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, amet!",
+      },
+    ],
   },
 });
