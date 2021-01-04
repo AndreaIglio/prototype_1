18a90cb945e4aa69171a3d5a3f659e868dc517ab
Andrea Iglio <andrea.iglio@gmail.com>
2021-01-04 17:14:34 +0100

    Implemented VueJs
    Implemented VueJs for "skills"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
-// Codice viene eseguito solo quando documento e' carico
-$(function () {
- 
+var app = new Vue({
+  el: "#root",
+  data: {
+    card_interests: "card_interests",
+    skills: [
+      "assets/img/skills/html5.svg",
+      "assets/img/skills/css3.svg",
+      "assets/img/skills/javascript.svg",
+      "assets/img/skills/jquery-1.svg",
+      "assets/img/skills/vue-9.svg",
+      "assets/img/skills/bootstrap-4.svg",
+    ],
+  },
 });
